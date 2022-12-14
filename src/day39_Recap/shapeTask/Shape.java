3581eaec516418a9647e2ccafaca942511438046
mustafa <mtf.m114@gmail.com>
2022-12-14 10:28:36 +0300

    Recap

---
+++ src/day39_Recap/shapeTask/Shape.java
@@ -0,0 +1,40 @@
+package day39_Recap.shapeTask;
+
+public class Shape {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name==null){
+            System.err.println("name can NOT be null");
+            System.exit(1);// something went wrong
+        }
+
+        this.name = name;
+    }
+
+
+    // dont make it private method . other class cant use or overriding
+    public Shape(String name) {
+        setName(name);
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+
+
+
+
+
+
+}

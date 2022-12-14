1f18d2183d160eda6dd1e33852792e1dcce2a3c2
mustafa <mtf.m114@gmail.com>
2022-12-14 13:12:04 +0300

    create shape object

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,82 @@
+package day39_Recap.cydeoTask;
+
+import java.util.IllegalFormatCodePointException;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            System.err.println("Invalid name " + name);
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            System.err.println("Invalid age " + age);
+            System.exit(1);
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if (!(gender == 'M' || gender == 'f')) {
+            System.err.println("Invalid gender" + gender);
+            System.exit(1);
+        }
+        this.gender = gender;
+    }
+
+
+
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+
+
+
+    public void eat(){
+        System.out.println(name +" is eating ");
+    }
+
+    public void drink(){
+        System.out.println(name +" is drink");
+    }
+
+    public void sleep(){
+        System.out.println(name +" is sleeping");
+    }
+
+
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+
+}
